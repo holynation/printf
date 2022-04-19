bab2c3678258eb27fd21a48186321717cb1192de
Alatise Oluwaseun <holynation667@gmail.com>
2022-04-19 16:11:44 +0100

    modify how to write to std

--- _putchar.c
+++ _putchar.c
@@ -12,3 +12,20 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * create_buffer - prints buffer and free it and free va_lis
+ * @buffer: buffer holding print-ables
+ * @len: length of print-able string
+ * @list: va_list
+ */
+
+void create_buffer(char *buffer, int len, va_list list)
+{
+    char *buff; /* array of characters */
+
+    buff = realloc(buffer, len); /* realloc to correct size */
+    write(1, buff, len); /* print to stdout */
+
+    free(buff); va_end(list); /* cleanup memory */
+}
