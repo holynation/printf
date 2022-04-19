3a710ba640bbe01c0ded856a460c4fab5fdee7ca
heeyahnuh123 <heeyahnuh@gmail.com>
2022-04-19 14:45:57 -0700

    commit

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - prints length string in integer
+ * @s: string passed to function
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*(s + a))
+		a++;
+
+	return (dest);
+}
